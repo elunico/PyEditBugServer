3fe2de7f0f30cdc2f462180c452f86d3777297b9
Thomas Povinelli <tompov227@gmail.com>
2019-03-26 13:43:19 -0400

    This is my own fault for being hopeful

--- server.js
+++ server.js
@@ -7,9 +7,7 @@ server.use(express.json());
 
 const port = process.env.PORT || 8099;
 
-const {
-  Client
-} = require('pg');
+const {Client} = require('pg');
 
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
@@ -56,7 +54,7 @@ server.listen(port);
 
 console.log(`Listening on ${port}`);
 
-server.post('/bug-report', function (req, res) {
+server.post('/bug-report', function(req, res) {
   var report = {
     'params': {
       'created': req.body.parameters.created.replace('\n', ' '),
@@ -76,8 +74,7 @@ server.post('/bug-report', function (req, res) {
     }
   };
   const queryTemplate =
-    'INSERT into bugs (created, platform, version, pyversion, user_user, steps, info, preferences, log, appname) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);',
-  ;
+      'INSERT into bugs (created, platform, version, pyversion, user_user, steps, info, preferences, log, appname) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);';
   const parameters = [
     Date.parse(report.params.created), report.params.platform,
     report.params.version, report.params.pyversion, report.user.user,
@@ -86,20 +83,14 @@ server.post('/bug-report', function (req, res) {
   ];
   client.query(queryTemplate, parameters, (err, sqlresp) => {
     if (err) {
-      res.writeHead(500, {
-        'Content-Type': 'text/plain',
-        'Success': 'false'
-      });
+      res.writeHead(500, {'Content-Type': 'text/plain', 'Success': 'false'});
       res.write('' + JSON.stringify(sqlresp));
       res.end();
       throw err;
     }
     for (let row of res.rows) {
       console.log(JSON.stringify(row));
-      res.writeHead(200, {
-        'Content-Type': 'text/plain',
-        'Success': 'true'
-      });
+      res.writeHead(200, {'Content-Type': 'text/plain', 'Success': 'true'});
       res.write('Successfully submitted bug report!');
       res.end();
     }
